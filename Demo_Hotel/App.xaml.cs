7944b184f7995d32362530daca31ca985cb116f3
Ivan Krysenko <assuss2120@gmail.com>
2024-10-29 11:14:14 +0300

    Добавьте файлы проекта.

---
+++ Demo_Hotel/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Demo_Hotel.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demo_Hotel
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Представляет контекст данных для взаимодействия с базой данных
+        /// </summary>
+        public static HotelEntities context = new HotelEntities();
+    }
+}
