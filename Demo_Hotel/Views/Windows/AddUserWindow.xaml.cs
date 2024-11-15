21b6d672d51087684312c1eee8e46a146e39cacc
Ivan Krysenko <assuss2120@gmail.com>
2024-11-15 14:19:44 +0300

    Окно с добавлением пользователя

---
+++ Demo_Hotel/Views/Windows/AddUserWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo_Hotel.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddUserWindow.xaml
+    /// </summary>
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
